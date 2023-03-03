00b43c6e5dc9b48b17151b60cc605e18a764f38c
JennyW <jiongw22@gmail.com>
2023-03-02 16:44:15 -0800

    updated note

--- note.py
+++ note.py
@@ -154,3 +154,29 @@ def pingpong(n):
 
 # The count coins function (a different kind of the count_partitions problem)
 
+Lecture 14 Tree
+def tree(label, branches = []):
+    for branch in branches:
+        assert is_tree(branch), 'branches must be trees'
+    return [label] + list(branches)
+
+def label(tree):
+    return tree[0]
+
+def branches(tree):
+    return [tree1:]
+
+def is_tree(tree):
+    if type(tree) != list or len(tree) < 1:
+        return False
+    for branch in branches(tree):
+        if not is_tree(branch):
+            return False
+    return True
+
+def is_leaf(tree):
+    return not branches(tree)
+    
+def fib_tree(n):
+    if n <= 1:
+        return tree(n)
